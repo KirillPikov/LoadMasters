22b2ac822ce03ed0f4aeea435ee2fb0c6bfc78f5
KirillPikov <pikov_kirya@bk.ru>
2019-11-25 19:01:09 +0400

    Update Airport.java

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,31 +1,38 @@
+import javafx.util.Pair;
+
 import java.util.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Airport {
     /** Основная очередь грузов. */
-    private Queue<Cargo> mainCargoQueue;
+    private volatile Queue<Cargo> mainCargoQueue;
 
-    /** Очередь грузов, для которых на данный момент нет самолётов. */
-    private Queue<Cargo> garageCargoQueue;
+    /** Очередь грузов, для которых на не нашлось самолёта. */
+    private volatile Queue<Cargo> garageCargoQueue;
 
     /** Очередь доступных самолётов. */
-    private Queue<Plane> planeQueue;
+    private volatile List<Plane> planeQueue;
 
     public Airport() {
         mainCargoQueue = new ArrayDeque<>();
         garageCargoQueue = new ArrayDeque<>();
-        planeQueue = new ArrayDeque<>();
+        planeQueue = new ArrayList<>();
     }
-
+        //TODO Не убавляется freeVolume в отсеке
     public void startWork() throws InterruptedException {
+        Executor executor = Executors.newFixedThreadPool(3);
         Timer timer = new Timer();
         TimerTask cargoGenerator = new CargoGenerator(mainCargoQueue);
-        timer.schedule(cargoGenerator, 0, 1000);
+        timer.schedule(cargoGenerator, 0, 500);
         TimerTask planeGenerator = new PlaneGenerator(planeQueue);
-        timer.schedule(planeGenerator, 0, 1000);
+        timer.schedule(planeGenerator, 0, 500);
         while (true) {
-            Thread.sleep(1000);
-            System.out.println(Arrays.toString(mainCargoQueue.toArray()));
-            System.out.println(Arrays.toString(planeQueue.toArray()));
+            executor.execute(new LoadMaster(mainCargoQueue, garageCargoQueue, planeQueue));
+       //     System.out.println(Arrays.toString(mainCargoQueue.toArray()));
+        //    System.out.println(Arrays.toString(planeQueue.toArray()));
+            Thread.sleep(500);
         }
     }
 }
