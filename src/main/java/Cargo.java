630a9a1ed13760d5f22a85c2dd0ba368f3ea320b
KirillPikov <pikov_kirya@bk.ru>
2019-11-25 16:21:22 +0400

    Update Cargo.java

--- src/main/java/Cargo.java
+++ src/main/java/Cargo.java
@@ -2,23 +2,25 @@
  * Груз.
  */
 public class Cargo {
-    /**
-     * Объём груза.
-     */
+
+    /** Объём груза. */
     private final int volume;
-    /**
-     * Тип груза.
-     */
+
+    /** Тип груза. */
     private final CargoType type;
+
+    /** Место назначения груза. */
+    private final Destination destination;
 
     /**
      * Конструктор с параметрами.
      * @param volume {@link #volume}
      * @param type {@link #type}
      */
-    public Cargo(int volume, CargoType type) {
+    public Cargo(int volume, CargoType type, Destination destination) {
         this.volume = volume;
         this.type = type;
+        this.destination = destination;
     }
 
     public int getVolume() {
@@ -27,5 +29,18 @@ public class Cargo {
 
     public CargoType getType() {
         return type;
+    }
+
+    public Destination getDestination() {
+        return destination;
+    }
+
+    @Override
+    public String toString() {
+        return "Cargo{" +
+                "volume=" + volume +
+                ", type=" + type +
+                ", destination=" + destination +
+                '}';
     }
 }
