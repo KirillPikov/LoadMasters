5d70b5fb9e5c39eaa566b9f3bc6efdb511f5f4ec
KirillPikov <pikov_kirya@bk.ru>
2019-11-25 16:21:31 +0400

    Create Destination.java

---
+++ src/main/java/Destination.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Место назначения грузов и самолётов.
+ */
+public enum Destination {
+
+    /** Анапа. */
+    ANAPA,
+
+    /** Сургут. */
+    SYRGYT,
+
+    /** Сочи. */
+    SOCHI,
+
+    /** Москва. */
+    MOSKOW,
+
+    /** * Ульяновск. */
+    YLSK;
+
+    private static final List<Destination> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static Destination getRandomDestination()  {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
+}
