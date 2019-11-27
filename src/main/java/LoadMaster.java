b55189780838f6e5e45c851512d3118fea8735c8
KirillPikov <pikov_kirya@bk.ru>
2019-11-27 14:41:16 +0400

    Update LoadMaster.java

--- src/main/java/LoadMaster.java
+++ src/main/java/LoadMaster.java
@@ -1,4 +1,3 @@
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import javafx.util.Pair;
 
 import java.util.List;
@@ -22,36 +21,86 @@ public class LoadMaster implements Runnable {
     }
 
     @Override
-    public synchronized void run() {
-        System.out.println("Thread " + this);
+    public void run() {
+        System.out.println(this + " Начало нового выполнения LoadMaster ");
         Cargo cargo = null;
+        Pair<Boolean, Cargo> resultPut = null;
+
+        synchronized (garageCargoQueue) {
+            if(!garageCargoQueue.isEmpty()) {
+                System.out.println(this + " Взял груз из доп склада");
+                cargo = garageCargoQueue.poll();
+                if(cargo != null) {
+                    resultPut = this.tryPutCargo(cargo);
+                }
+            }
+            if(resultPut == null || (!resultPut.getKey() && resultPut.getValue() == null)) {
+                if(cargo != null && resultPut == null) {
+                    garageCargoQueue.add(cargo);
+                }
+                synchronized (mainCargoQueue) {
+                    System.out.println(this + " На доп складе нет груза");
+                    if (mainCargoQueue.isEmpty()) {
+                        try {
+                            System.out.println(this + " Основной склад пуст. Перехожу в ожидание.");
+                            mainCargoQueue.wait();
+                            System.out.println(this + " Дождался нового груза на основном складе");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    cargo = mainCargoQueue.poll();
+                    System.out.println(this + " Получил груз с основного склада");
+                }
+            }
+        }
+
+        System.out.println(this + " Резултат загрузки груза " + resultPut);
+        if(resultPut != null && !resultPut.getKey()) {
+            synchronized (garageCargoQueue) {
+                if (resultPut.getValue() != null) {
+                    System.out.println(this + " Груз добавился с заменой " + resultPut);
+                    garageCargoQueue.add(resultPut.getValue());
+                }
+            }
+        }
+        if(resultPut == null && cargo != null) {
+            System.out.println(this + " Груз не получилось добавить, возвращаем его ");
+            synchronized (garageCargoQueue) {
+                garageCargoQueue.add(cargo);
+            }
+        }
+    }
+
+    private Pair<Boolean, Cargo> tryPutCargo(Cargo cargo) {
         Plane plane = null;
         Pair<Boolean, Cargo> resultPut = null;
-        if(!garageCargoQueue.isEmpty()) {
-            cargo = garageCargoQueue.poll();
-        }
-        else if(!mainCargoQueue.isEmpty()) {
-            cargo = mainCargoQueue.poll();
-        }
-        if(cargo != null) {
+        synchronized (planeQueue) {
+            if(planeQueue.isEmpty()) {
+                try {
+                    System.out.println(this + " Очередь самолётов пуста, перехожу в ожидание");
+                    planeQueue.wait();
+                    System.out.println(this + " Дождался нового самолёта");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             for (int i = 0; i < planeQueue.size(); i++) {
                 plane = planeQueue.get(i);
                 if(plane.canTransferCargo(cargo)) {
-                    System.out.println("OK!K!");
+                    System.out.println(this + " Можем транспортировать ");
                     resultPut = plane.putCargo(cargo);
-                    System.out.println(resultPut);
                     break;
                 }
-
             }
         }
-        System.out.println(resultPut);
-        if(resultPut != null && !resultPut.getKey()) {
-            garageCargoQueue.add(resultPut.getValue());
+        if(resultPut != null && plane != null && plane.isReady()) {
+            System.out.println(this + " Самолёт готов к отправке, мест нет! " + plane);
+            synchronized (planeQueue) {
+                planeQueue.remove(plane);
+            }
         }
-        if(plane != null && !plane.isReady()) {
-            System.out.println("ADD!");
-            planeQueue.add(plane);
-        }
+        return resultPut;
     }
+
 }
