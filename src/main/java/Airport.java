37be5d163630072aacc89f9439c40186b35f40cc
KirillPikov <pikov_kirya@bk.ru>
2019-11-28 16:20:20 +0400

    Update Airport.java

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Airport {
@@ -12,25 +12,32 @@ public class Airport {
     /** Очередь доступных самолётов. */
     private volatile List<Plane> planeQueue;
 
+    /** Конструктор без параметров. */
     public Airport() {
         mainCargoQueue = new ArrayDeque<>();
         garageCargoQueue = new ArrayDeque<>();
         planeQueue = new ArrayList<>();
     }
 
-    public synchronized void startWork() throws InterruptedException {
-        Executor executor = Executors.newFixedThreadPool(30);
+    public void startWork() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(Settings.LOAD_MASTER_COUNT);
         Timer timer = new Timer();
         TimerTask cargoGenerator = new CargoGenerator(mainCargoQueue);
-        timer.schedule(cargoGenerator, 0, 1000);
+        timer.schedule(cargoGenerator, 0, Settings.CARGO_GENERATING_PERIOD);
         TimerTask planeGenerator = new PlaneGenerator(planeQueue);
-        timer.schedule(planeGenerator, 0, 20000000);
+        timer.schedule(planeGenerator, 0, Settings.PLANE_GENERATING_PERIOD);
         while (true) {
-            executor.execute(new LoadMaster(mainCargoQueue, garageCargoQueue, planeQueue));
-            System.out.println(this + " garageQueue: " + Arrays.toString(garageCargoQueue.toArray()));
-            System.out.println(this + " mainCargoQueue: " + Arrays.toString(mainCargoQueue.toArray()));
-            System.out.println(this + " planeQueue: " + Arrays.toString(planeQueue.toArray()));
-            Thread.sleep(1000);
+            executorService.execute(new LoadMaster(mainCargoQueue, garageCargoQueue, planeQueue));
+            synchronized (garageCargoQueue) {
+                System.out.println(this + " garageQueue: "    + Arrays.toString(garageCargoQueue.toArray()));
+            }
+            synchronized (mainCargoQueue) {
+                System.out.println(this + " mainCargoQueue: " + Arrays.toString(mainCargoQueue.toArray()));
+            }
+            synchronized (planeQueue) {
+                System.out.println(this + " planeQueue: "     + Arrays.toString(planeQueue.toArray()));
+            }
+            Thread.sleep(Settings.LOAD_MASTERS_CALL_PERIOD);
         }
     }
 }
